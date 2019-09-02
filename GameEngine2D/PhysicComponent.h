b7eaf96c167f0743505266fdc6a03a734dc80c2e
kali cops <gabriel.plante@live.fr>
2019-09-02 22:22:49 +0200

    #3 Improvement on physic component

--- GameEngine2D/PhysicComponent.h
+++ GameEngine2D/PhysicComponent.h
@@ -1,5 +1,19 @@
 #pragma once
-class PhysicComponent
+struct PhysicComponent
 {
+private:
+	const long ownerEntityID;
+public:
+	//The actual attribute are public because it's not the component that change them, it's the system
+	long x{ 0 };
+	long y{ 0 };
+
+	//Constructor
+	PhysicComponent(const long ownerEntityID)
+		:ownerEntityID{ ownerEntityID }
+	{}
+
+	//Get the entity ID of the owner of this component
+	long getOwnerEntityID() const { return ownerEntityID; }
 };
 
--- GameEngine2D/PhysicComponentFactory.h
+++ GameEngine2D/PhysicComponentFactory.h
@@ -1,5 +1,16 @@
 #pragma once
+#include <vector>
+
+#include "PhysicComponent.h"
+
 class PhysicComponentFactory
 {
+private:
+	std::vector<PhysicComponent> physicComponentList;
+public:
+	//Add a physic component to the list 
+	void addPhysicComponent(const long ownerEntityID) { physicComponentList.push_back(PhysicComponent{ ownerEntityID }); }
+
+	//std::iterator<std::vector<PhysicComponent>, > 
 };
 
