9c302ad1b258fe28914f147fa82bf124c7094c25
kali cops <gabriel.plante@live.fr>
2019-09-17 23:54:25 +0200

    #3 Created an IDGenerator, updated the factory

--- GameEngine2D/Factory.h
+++ GameEngine2D/Factory.h
@@ -1,14 +1,25 @@
 #pragma once
 #include <vector>
 
-//Factory is the template class that will hold every component of a component type
+#include "IDGenerator.h"
+
+/*
+ * Factory is the template class that will hold every component of a component type
+ * The id given after adding a component is garanted not to change
+*/
 template <typename Component>
 class Factory
 {
 private:
+	IDGenerator<> iDGenerator;
 	std::vector<Component> componentList;
 public:
-	//Add a component to the list
-	void addComponent(Component component) { componentList.push_back(component); }
+	//Add a component to the list, and return an id for the component
+	int addComponent(Component component) {
+		componentList.push_back(component);
+		return iDGenerator.getNewID();
+	}
+	
+	//TODO add the delete component method
 };
 
---
+++ GameEngine2D/IDGenerator.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <vector>
+
+/*
+ * IDGenerator give you an valid and unique ID
+*/
+template <typename T = unsigned int>
+class IDGenerator {
+private:
+	std::vector<T> deletedID;
+	T maxUsedID{ 0 };
+public:
+	//Get a ID that is valid and not used
+	T getNewID() {
+		if (deletedID.empty()) {
+			maxUsedID++;
+			return maxUsedID;
+		}
+		else {
+			id = deletedID.back();
+			deletedID.pop_back();
+			return id;
+		}
+	}
+
+	//Delete an id, return true if the id has been deleted
+	bool deleteID(T id) {
+		//Check to see if the id is valid
+		if (id > maxUsedID)
+			return false;
+		//Check to see if it is not already deleted
+		for (T deletedId : deletedID) {
+			if (deletedId == id) {
+				return false
+			}
+		}
+		//Add it to the list
+		deletedID.push_back(it);
+		return true;
+	}
+};
