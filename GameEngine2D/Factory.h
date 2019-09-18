d155b2a79b8d586284acb6b5150462acfdc12c4a
GabrielPlante <gabriel.plante@live.fr>
2019-09-18 16:35:40 +0200

    #7 added get component to the factory and added id support in addcomponent

--- GameEngine2D/Factory.h
+++ GameEngine2D/Factory.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <unordered_map>
 
 #include "IDGenerator.h"
 
@@ -13,13 +14,28 @@ class Factory
 private:
 	IDGenerator<> iDGenerator;
 	std::vector<Component> componentList;
+	std::unordered_map<unsigned int, unsigned int> iDtoPlace;
 public:
 	//Add a component to the list, and return an id for the component
 	int addComponent(Component component) {
+		//Push the component in the list
 		componentList.push_back(component);
-		return iDGenerator.getNewID();
+
+		//Get the ID for this component
+		unsigned int id = iDGenerator.getNewID();
+		//Insert ID in the map
+		iDtoPlace.insert(std::make_pair<unsigned int, unsigned int>(id, componentList.size()));
+		return id;
+	}
+
+	Component* getComponent(unsigned long id) const {
+		return &componentList[iDtoPlace.find(id)->second()];
 	}
 	
 	//TODO add the delete component method
+	void deleteComponent(unsigned long id) {
+
+	}
+
 };
 
