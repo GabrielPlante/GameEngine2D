02a3fb5fb92810bea594156fc7dcfe425798cad9
kali cops <gabriel.plante@live.fr>
2020-10-05 22:08:50 +0200

    #36 Removed Ianagd, updated dependencies

--- Ianagd/MessageOrigin.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-//Here are defined (with constexpr) every message origin of the game
-//The type of the message origin is unsigned type
-
-namespace ian {
-	constexpr unsigned int gameCore{ 0 };
-}
--- Ianagd/MessageType.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-//Here are defined (with constexpr) every message type of the game
-//The type of the message type is unsigned type
-
-namespace ian {
-	constexpr unsigned int createPhysicComponent{ 0 };
-}
